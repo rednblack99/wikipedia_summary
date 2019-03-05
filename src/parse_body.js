8a00cf8086dfdc5df3d6d38a40f2dfad0731e9eb
Adam Lodowski <adamlodowski@gmail.com>
2019-03-05 12:11:27 +0000

    Not functional: attempted isolating bday class. Commiting for reference

--- src/parse_body.js
+++ src/parse_body.js
@@ -6,8 +6,18 @@ class ParseBody {
   parse_page() {
     let parser = new DOMParser();
     let doc = parser.parseFromString(this.page_body, "text/html");
-    console.log(doc)
-    return doc
+    console.log(doc.body)
+    // console.log(doc.body.children)
+    // let parser_output = doc.body.children
+    // console.log(parser_output[0].children)
+    // let body = parser_output[0].children
+    // console.log(body.item(2).children)
+    // let infobox = body[2].children
+    // console.log(infobox.item(2))
+    // let tbody = infobox.item(2)
+
+    console.log(doc.querySelectorAll(".bday"))
+    console.log(doc.getElementsByClassName("/infobox"))
   }
 
 }
